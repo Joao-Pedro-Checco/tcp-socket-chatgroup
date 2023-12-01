c9f884ca8cd0b6f9dbd3a59fdfa325f3a445e5e7
unknown <joaochecco@gmail.com>
2023-12-01 14:49:33 -0300

    server e handler de client

--- src/main/java/src/ClientHandler.java
+++ src/main/java/src/ClientHandler.java
@@ -1,4 +1,76 @@
 package src;
 
-public class ClientHandler {
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable {
+    private static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    private BufferedReader in;
+    private BufferedWriter out;
+    private String username;
+
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.username = in.readLine();
+            clientHandlers.add(this);
+            broadcastMessage("Server: " + username + " se conectou!");
+        } catch (IOException e) {
+            closeEverything(socket, in, out);
+        }
+    }
+
+    @Override
+    public void run() {
+        String clientMessage;
+        while (socket.isConnected()) {
+            try {
+                clientMessage = in.readLine();
+                broadcastMessage(clientMessage);
+            } catch (IOException e) {
+                closeEverything(socket, in, out);
+                break;
+            }
+        }
+    }
+
+    public void broadcastMessage(String message) {
+        for (ClientHandler clientHandler : clientHandlers) {
+            try {
+                if (clientHandler != this) {
+                    clientHandler.out.write(message);
+                    clientHandler.out.newLine();
+                    clientHandler.out.flush();
+                }
+            } catch (IOException e) {
+                closeEverything(socket, in, out);
+            }
+        }
+    }
+
+    public void removeClientHandler() {
+        clientHandlers.remove(this);
+        broadcastMessage("Server: " + username + " desconectou!");
+    }
+
+    public void closeEverything(Socket socket, BufferedReader in, BufferedWriter out) {
+        removeClientHandler();
+        try {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/src/Server.java
+++ src/main/java/src/Server.java
@@ -1,4 +1,43 @@
 package src;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void start() {
+        try {
+            while (!serverSocket.isClosed()) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Um novo cliente entrou!");
+                ClientHandler handler = new ClientHandler(clientSocket);
+                Thread thread = new Thread(handler);
+                thread.start();
+            }
+        } catch (IOException e) {
+            closeServerSocket();
+        }
+    }
+
+    public void closeServerSocket() {
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket socket = new ServerSocket(8080);
+        Server server = new Server(socket);
+        server.start();
+    }
 }
