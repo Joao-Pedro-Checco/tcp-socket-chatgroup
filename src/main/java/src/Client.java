a04abac7ac67001833fa62062c78f41b0a80c180
unknown <joaochecco@gmail.com>
2023-12-01 14:50:00 -0300

    client

--- src/main/java/src/Client.java
+++ src/main/java/src/Client.java
@@ -1,4 +1,85 @@
 package src;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+    private Socket socket;
+    private BufferedReader in;
+    private BufferedWriter out;
+    private String username;
+
+    public Client(Socket socket, String username) {
+        try {
+            this.socket = socket;
+            this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.username = username;
+        } catch (IOException e) {
+            closeEverything(socket, in, out);
+        }
+    }
+
+    public void sendMessage() {
+        try {
+            out.write(username);
+            out.newLine();
+            out.flush();
+
+            Scanner scanner = new Scanner(System.in);
+            while (socket.isConnected()) {
+                String message = scanner.nextLine();
+                out.write(message);
+                out.newLine();
+                out.flush();
+            }
+        } catch (IOException e) {
+            closeEverything(socket, in, out);
+        }
+    }
+
+    public void listenForMessage() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String groupChatMessage;
+                while (socket.isConnected()) {
+                    try {
+                        groupChatMessage = in.readLine();
+                        System.out.println(groupChatMessage);
+                    } catch (IOException e) {
+                        closeEverything(socket, in, out);
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void closeEverything(Socket socket, BufferedReader in, BufferedWriter out) {
+        try {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um nome de usuario:");
+        String username = scanner.nextLine();
+
+        Socket clientSocket = new Socket("localhost", 8080);
+        Client client = new Client(clientSocket, username);
+        client.listenForMessage();
+        client.sendMessage();
+    }
 }
